6c8086dcb287787489a0eaa8484380021a3a661e
Marcin Laskowski <laskowski.m.m+github@gmail.com>
2021-04-04 12:35:30 +0200

    ex 2 (almost resolved)

--- sandbox/src/main/java/pl/stqa/mla/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/pl/stqa/mla/sandbox/MyFirstProgram.java
@@ -1,30 +1,36 @@
 package pl.stqa.mla.sandbox;
 
 public class MyFirstProgram {
-
   public static void main(String[] args) {
     hello("universe");
     hello("user");
     hello("Alexei");
 
-    double l = 5;
-    System.out.println("Powierzchnia kwadratu o boku " + l + " = " + area(l));
+    Square s = new Square(5);
+    System.out.println("Powierzchnia kwadratu o boku " + s.l + " = " + s.area());
 
-    double a = 4;
-    double b = 6;
-    System.out.println("Powierzchnia prostokąta o bokach " + a + " i " + b + " = " + area(a, b));
+    Rectangle r = new Rectangle(4, 6);
+    System.out.println("Powierzchnia prostokąta o bokach " + r.a + " i " + r.b + " = " + r.area());
+
+    double p1x = 1;
+    double p1y = 1;
+    double p2x = 4;
+    double p2y = 4;
+    System.out.println(Math.sqrt((p2x - p1x) * (p2x - p1x) + (p2y - p1y) * (p2y - p1y)));
+
+    System.out.println("Odległość między punktami: P1(" + p1x + "," + p1y + ") i P2(" + p2x + "," + p2y + ") - wynosi: " + Math.sqrt((p2x - p1x) * (p2x - p1x) + (p2y - p1y) * (p2y - p1y)));
+
+    Point p1 = new Point(1,1);
+    Point p2 = new Point(4,4);
+    System.out.println("Odległość między punktami: P1(" + p1.x + "," + p1.y + ") i P2(" + p2.x + "," + p2.y + ") - wynosi: " + distance(p1, p2));
   }
 
   public static void hello(String somebody) {
     System.out.println("Hello, " + somebody + "!");
   }
 
-  public static double area(double len) {
-    return len * len;
-  }
-
-  public static double area(double a, double b) {
-    return a * b;
+  public static double distance(Point p1, Point p2) {
+    return Math.sqrt((p2.x - p1.x)*(p2.x - p1.x)+(p2.y - p1.y)*(p2.y - p1.y));
   }
 
 }
---
+++ sandbox/src/main/java/pl/stqa/mla/sandbox/Square.java
@@ -0,0 +1,15 @@
+package pl.stqa.mla.sandbox;
+
+public class Square {
+
+  public double l;
+
+  public Square(double l) {
+    this.l = l;
+  }
+
+  public double area() {
+    return this.l * this.l;
+  }
+
+}
