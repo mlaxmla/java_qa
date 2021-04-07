b1510b202caa5a686a3647255cb73d2a1fcd1152
Marcin Laskowski <laskowski.m.m+github@gmail.com>
2021-04-07 10:32:24 +0200

    Multilevel architecture 2 Groups' Tests and 1 Contact Test

--- addressbook-web-tests/src/test/java/pl/stqa/mla/addressbook/ContactCreationTest.java
+++ addressbook-web-tests/src/test/java/pl/stqa/mla/addressbook/ContactCreationTest.java
@@ -1,103 +1,17 @@
 package pl.stqa.mla.addressbook;
 
-import java.util.concurrent.TimeUnit;
 import org.testng.annotations.*;
-import org.openqa.selenium.*;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class ContactCreationTest {
-  private WebDriver wd;
-
-  @BeforeMethod(alwaysRun = true)
-  public void setUp() throws Exception {
-    wd = new FirefoxDriver();
-    wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-    gotoHomePage();
-    login("admin", "secret");
-  }
-
-  private void login(String username, String password) {
-    wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys(username);
-    wd.findElement(By.name("pass")).clear();
-    wd.findElement(By.name("pass")).sendKeys(password);
-    wd.findElement(By.xpath("//input[@value='Login']")).click();
-  }
+public class ContactCreationTest extends TestBase {
 
   @Test
   public void testContactCreation() throws Exception {
     gotoHomePage();
     initContactCreation();
     fillContactForm(new ContactData("FirstName_TestData", "MiddleNameTestData", "LastName_TestData", "Nickname_TestData", "Title_TestData", "Company_TestData", "Address_TestData", "TelHome_testData", "TelMobile_testData", "email_testData", "email2_testData", "Homepage_testData"));
-    subminContactCreation();
+    submitContactCreation();
     returnToHomePage();
 
-  }
-
-  private void gotoHomePage() {
-    wd.get("http://localhost/addressbook/");
-  }
-
-  private void returnToHomePage() {
-    wd.findElement(By.linkText("home page")).click();
-  }
-
-  private void subminContactCreation() {
-    wd.findElement(By.name("submit")).click();
-  }
-
-  private void fillContactForm(ContactData contactData) {
-    wd.findElement(By.name("firstname")).clear();
-    wd.findElement(By.name("firstname")).sendKeys(contactData.getFirstName_td());
-    wd.findElement(By.name("middlename")).clear();
-    wd.findElement(By.name("middlename")).sendKeys(contactData.getMiddleName_td());
-    wd.findElement(By.name("lastname")).clear();
-    wd.findElement(By.name("lastname")).sendKeys(contactData.getLastName_td());
-    wd.findElement(By.name("nickname")).clear();
-    wd.findElement(By.name("nickname")).sendKeys(contactData.getNickName_td());
-    wd.findElement(By.name("title")).clear();
-    wd.findElement(By.name("title")).sendKeys(contactData.getTitle_td());
-    wd.findElement(By.name("company")).clear();
-    wd.findElement(By.name("company")).sendKeys(contactData.getCompany_td());
-    wd.findElement(By.name("address")).clear();
-    wd.findElement(By.name("address")).sendKeys(contactData.getAddress_td());
-    wd.findElement(By.name("home")).clear();
-    wd.findElement(By.name("home")).sendKeys(contactData.getTelHome_td());
-    wd.findElement(By.name("mobile")).clear();
-    wd.findElement(By.name("mobile")).sendKeys(contactData.getTelMobile_td());
-    wd.findElement(By.name("email")).clear();
-    wd.findElement(By.name("email")).sendKeys(contactData.getEmail_td());
-    wd.findElement(By.name("email2")).clear();
-    wd.findElement(By.name("email2")).sendKeys(contactData.getEmail2_td());
-    wd.findElement(By.name("homepage")).clear();
-    wd.findElement(By.name("homepage")).sendKeys(contactData.getHomePage_td());
-  }
-
-  private void initContactCreation() {
-    wd.findElement(By.linkText("add new")).click();
-  }
-
-  @AfterMethod(alwaysRun = true)
-  public void tearDown() throws Exception {
-    wd.quit();
-  }
-
-  private boolean isElementPresent(By by) {
-    try {
-      wd.findElement(by);
-      return true;
-    } catch (NoSuchElementException e) {
-      return false;
-    }
-  }
-
-  private boolean isAlertPresent() {
-    try {
-      wd.switchTo().alert();
-      return true;
-    } catch (NoAlertPresentException e) {
-      return false;
-    }
   }
 
 }
---
+++ addressbook-web-tests/src/test/java/pl/stqa/mla/addressbook/GroupDeletionTest.java
@@ -0,0 +1,15 @@
+package pl.stqa.mla.addressbook;
+
+import org.testng.annotations.Test;
+
+public class GroupDeletionTest extends TestBase {
+
+  @Test
+  public void testGroupDeletion() {
+    gotoGroupPage();
+    selectGroup();
+    deleteSelectedGroups();
+    returnToGroupPage();
+  }
+
+}
