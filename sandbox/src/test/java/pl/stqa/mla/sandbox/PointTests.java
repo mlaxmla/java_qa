9f503b935cd2113d634dd5221bcbacd996ea5d6b
Marcin Laskowski <laskowski.m.m+github@gmail.com>
2021-04-08 23:19:32 +0200

    ex 2 (correction, still needs improvemement)

--- sandbox/src/test/java/pl/stqa/mla/sandbox/PointTests.java
+++ sandbox/src/test/java/pl/stqa/mla/sandbox/PointTests.java
@@ -9,10 +9,10 @@ public class PointTests {
   public void testDistance() {
     Point p1 = new Point(0,0);
     Point p2 = new Point(4,0);
-    Assert.assertEquals(MyFirstProgram.distance(p1,p2), 4);
+    Assert.assertEquals(MyFirstProgram.distanceOld(p1,p2), 4);
     Point p3 = new Point(0,4);
-    Assert.assertEquals(MyFirstProgram.distance(p1,p3), 4);
+    Assert.assertEquals(MyFirstProgram.distanceOld(p1,p3), 4);
     Point p4 = new Point(4,4);
-    Assert.assertEquals(MyFirstProgram.distance(p1,p4), Math.sqrt(Math.pow(p4.y-p1.y,2) + Math.pow(p4.x-p1.x,2)));
+    Assert.assertEquals(MyFirstProgram.distanceOld(p1,p4), Math.sqrt(Math.pow(p4.y-p1.y,2) + Math.pow(p4.x-p1.x,2)));
   }
 }
