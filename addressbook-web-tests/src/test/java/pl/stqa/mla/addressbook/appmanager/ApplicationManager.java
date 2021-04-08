b04774549d880420be3aae0c036c97ba12f76127
Marcin Laskowski <laskowski.m.m+github@gmail.com>
2021-04-08 11:02:58 +0200

    Class appmanager.SessionHelper created

--- addressbook-web-tests/src/test/java/pl/stqa/mla/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/pl/stqa/mla/addressbook/appmanager/ApplicationManager.java
@@ -12,6 +12,7 @@ import java.util.concurrent.TimeUnit;
 public class ApplicationManager {
   WebDriver wd;
 
+  private SessionHelper sessionHelper;
   private NavigationHelper navigationHelper;
   private GroupHelper groupHelper;
 
@@ -21,16 +22,10 @@ public class ApplicationManager {
     gotoHomePage();
     groupHelper = new GroupHelper(wd);
     navigationHelper = new NavigationHelper(wd);
-    login("admin", "secret");
+    sessionHelper = new SessionHelper(wd);
+    sessionHelper.login("admin", "secret");
   }
 
-  private void login(String username, String password) {
-    wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys(username);
-    wd.findElement(By.name("pass")).clear();
-    wd.findElement(By.name("pass")).sendKeys(password);
-    wd.findElement(By.xpath("//input[@value='Login']")).click();
-  }
 
   public void gotoHomePage() {
     wd.get("http://localhost/addressbook/");
