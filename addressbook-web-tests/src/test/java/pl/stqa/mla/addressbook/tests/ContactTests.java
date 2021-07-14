88616790037ecb9c2dac432f4589cdd4d129332f
Marcin Laskowski <laskowski.m.m+github@gmail.com>
2021-07-14 14:52:28 +0200

    ex12 Contact tests merged into ContactTests

--- addressbook-web-tests/src/test/java/pl/stqa/mla/addressbook/tests/ContactTests.java
+++ addressbook-web-tests/src/test/java/pl/stqa/mla/addressbook/tests/ContactTests.java
@@ -9,7 +9,31 @@ import java.util.stream.Collectors;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-public class ContactPhoneTests extends TestBase {
+public class ContactTests extends TestBase {
+
+  @Test
+  public void testContactEmails() {
+    app.gotoHomePage();
+    ContactData contact = app.contact().all().iterator().next();
+    ContactData contactInfoFromEditForm = app.contact().infoFromEditForm(contact);
+
+    assertThat(contact.getAllEmails(), equalTo(mergeEmails(contactInfoFromEditForm)));
+  }
+
+  @Test
+  public void testContactAddress() {
+    app.gotoHomePage();
+    ContactData contact = app.contact().all().iterator().next();
+    ContactData contactInfoFromEditForm = app.contact().infoFromEditForm(contact);
+
+    assertThat(contact.getAddress_td(), equalTo(contactInfoFromEditForm.getAddress_td()));
+  }
+
+  private String mergeEmails(ContactData contact) {
+    return Arrays.asList(contact.getEmail_td(), contact.getEmail2_td(), contact.getEmail3_td())
+            .stream().filter((s) -> ! s.equals(""))
+            .collect(Collectors.joining("\n"));
+  }
 
   @Test
   public void testContactPhones() {
@@ -27,7 +51,7 @@ public class ContactPhoneTests extends TestBase {
   private String mergePhones(ContactData contact) {
     return Arrays.asList(contact.getTelHome_td(), contact.getTelMobile_td(), contact.getTelWork_td())
             .stream().filter((s) -> ! s.equals(""))
-            .map(ContactPhoneTests::cleaned)
+            .map(ContactTests::cleaned)
             .collect(Collectors.joining("\n"));
   }
 
