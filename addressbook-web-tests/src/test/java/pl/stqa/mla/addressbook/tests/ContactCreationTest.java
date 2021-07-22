7628bc23dd8eda1d1db36b18f2421a84e778ef78
Marcin Laskowski <laskowski.m.m+github@gmail.com>
2021-07-22 08:19:40 +0200

    ex 15 added JSON (de/)serialization with Gson for contacts

--- addressbook-web-tests/src/test/java/pl/stqa/mla/addressbook/tests/ContactCreationTest.java
+++ addressbook-web-tests/src/test/java/pl/stqa/mla/addressbook/tests/ContactCreationTest.java
@@ -1,5 +1,7 @@
 package pl.stqa.mla.addressbook.tests;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.thoughtworks.xstream.XStream;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -21,7 +23,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class ContactCreationTest extends TestBase {
 
   @DataProvider
-  public Iterator<Object[]> validContacts() throws IOException {
+  public Iterator<Object[]> validContactsFromXml() throws IOException {
 //    List<Object[]> list = new ArrayList<Object[]>();
     BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.xml")));
     String xml = "";
@@ -39,7 +41,23 @@ public class ContactCreationTest extends TestBase {
     return contacts.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
   }
 
-  @Test (dataProvider = "validContacts")
+  @DataProvider
+  public Iterator<Object[]> validContactsFromJson() throws IOException {
+    //    List<Object[]> list = new ArrayList<Object[]>();
+    try (BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.json")))) {
+      String json = "";
+      String line = reader.readLine();
+      while (line != null) {
+        json += line;
+        line = reader.readLine();
+      }
+      Gson gson = new Gson();
+      List<ContactData> contacts = gson.fromJson(json, new TypeToken<List<ContactData>>(){}.getType()); // idiom List<ContactData>.class
+      return contacts.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
+    }
+  }
+
+  @Test (dataProvider = "validContactsFromJson")
   public void testContactCreation(ContactData contact) {
     app.gotoHomePage();
     Contacts before = app.contact().all();
